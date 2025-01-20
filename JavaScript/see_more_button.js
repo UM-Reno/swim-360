6dc2173ad4503c26d1c697baa9273104c79105c0
MarkFialovszky17 <mark.fialovszky@um.edu.mt>
2025-01-20 10:11:26 +0100

    mobile view see more button
    Added see more button to mobile view on about us page

---
+++ JavaScript/see_more_button.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.see-more-btn');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const content = this.previousElementSibling.querySelector('.more-info');
+
+            if (content) {
+                if (content.style.display === 'none' || content.style.display === '') {
+                    content.style.display = 'inline';
+                    this.textContent = 'See Less';
+                } else {
+                    content.style.display = 'none';
+                    this.textContent = 'See More';
+                }
+            }
+        });
+    });
+});
