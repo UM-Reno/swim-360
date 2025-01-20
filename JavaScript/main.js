2534de9d2e7c56f8a634885b2fe74935b4cb1cff
LordReno <reno-yuri.camilleri.20@um.edu.mt>
2025-01-20 13:26:04 +0100

    Mobile Friendly

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,24 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('nav');
-    const logo = document.querySelector('.logo');
+// Function to check if the device is a phone
+function isPhone() {
+    return window.innerWidth <= 768;
+}
 
-    if (window.scrollY > 10) {
-        navbar.style.top = '0vh';  // When scrolling down, set top to 0
-        navbar.style.height = '10vh';
-        navbar.style.borderTopWidth = '0';
-        logo.style.height = '10vh';
-    }
+if (!isPhone()) {  // Only add the scroll event listener if it's not a phone
+    window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('nav');
+        const logo = document.querySelector('.logo');
 
-    else {
-        navbar.style.top = '1vh';  // When at the top of the page, set top back to 1vh
-        navbar.style.height = '12vh';
-        navbar.style.borderTopWidth = '5px';
-        logo.style.height = '12vh';
-    }
-});
+        if (window.scrollY > 10) {
+            navbar.style.top = '0vh';  // When scrolling down, set top to 0
+            navbar.style.height = '10vh';
+            navbar.style.borderTopWidth = '0';
+            logo.style.height = '10vh';
+        }
+        else {
+            navbar.style.top = '1vh';  // When at the top of the page, set top back to 1vh
+            navbar.style.height = '12vh';
+            navbar.style.borderTopWidth = '5px';
+            logo.style.height = '12vh';
+        }
+    });
+}
