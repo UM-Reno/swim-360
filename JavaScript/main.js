68a9e9fa067ca6229be47f6717af4df850b7ff26
LordReno <reno-yuri.camilleri.20@um.edu.mt>
2025-01-20 11:21:12 +0100

    Visual Changes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,18 @@
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('nav');
+    const logo = document.querySelector('.logo');
 
     if (window.scrollY > 10) {
-      navbar.style.top = '0vh';  // When scrolling down, set top to 0
-      navbar.style.height = '10vh';
+        navbar.style.top = '0vh';  // When scrolling down, set top to 0
+        navbar.style.height = '10vh';
+        navbar.style.borderTopWidth = '0';
+        logo.style.height = '10vh';
     }
-    
+
     else {
-      navbar.style.top = '1vh';  // When at the top of the page, set top back to 1vh
-      navbar.style.height = '12vh';
+        navbar.style.top = '1vh';  // When at the top of the page, set top back to 1vh
+        navbar.style.height = '12vh';
+        navbar.style.borderTopWidth = '5px';
+        logo.style.height = '12vh';
     }
-  });
-  
+});
